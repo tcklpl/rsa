8b4d164e71989ed4be7e255bfd1d7c38d531b2d9
tcklpl <thewalkingplayoficialbr@gmail.com>
2019-11-08 10:33:55 +0000

    Comentado e implementado n, e, phi e uma precodificação

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #define ll long long
 
 /// Método de MDC por algorítmo de Euclides
@@ -17,6 +18,11 @@ ll mdc(ll a, ll b) {
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "hicpp-signed-bitwise"
+/// Divisão modular
+/// \param a Valor base
+/// \param n Expoente
+/// \param p Divisor
+/// \return A^n % p
 ll mod(ll a, unsigned ll n, ll p) {
     ll res = 1;
     a %= p;
@@ -30,11 +36,13 @@ ll mod(ll a, unsigned ll n, ll p) {
         n = n >> 1; // n/2
         a = (a * a) % p;
     }
-
     return res;
 }
 #pragma clang diagnostic pop
-
+/// Checa a primalidade de um número
+/// \param n O número a ser checado
+/// \param iterations O número de iterações a serem checadas
+/// \return 1 caso for primo e 0 caso não for
 int primo(ll n, int iterations) {
     ll teste;
     // casos básicos
@@ -61,13 +69,49 @@ int primo(ll n, int iterations) {
     return 1;
 }
 
+void precodificar(char *in, char *out) {
+    unsigned ll i, c;
+    unsigned ll tamanho = strlen(in);
+    out[0] = '\0';
+    for (i = 0; i < tamanho; i++) {
+        c = in[i];
+        sprintf(out, "%lld", c);
+    }
+    sprintf(out, "\0");
+    puts(out);
+}
+
 int main() {
 
-    long long teste;
+    // 'p' e 'q' são números primos escoolhidos aleatoriamente
+    // 'n' é pq
+    // 'phi' é (p - 1)(q - 1)
+    // 'e' é um número tal que (2 < e < phi) e 'e' e 'phi' são co-primos
+    ll p, q, n, phi, e;
     srandom(time(NULL));
+    printf("Gerando p...\n");
     do {
-        teste = random();
-    } while (!primo(teste, 5));
-    printf("%lld\n", teste);
+        p = random();
+    } while (!primo(p, 2));
+    printf("Gerando q...\n");
+    do {
+        q = random();
+    } while (!primo(q, 2) || p == q);
+    printf("Gerando n e phi...\n");
+    n = p * q;
+    phi = (p - 1) * (q - 1);
+    printf("Gerando e...\n");
+    // 'e' deve ser menor que 'phi'
+    for (e = 2; e < phi; e++) {
+        // 'e' deve ser co-primo com 'phi'
+        if (mdc(e, phi) == 1)
+            break;
+    }
+
+    printf("Chaves:\n\t%lld\n\t%lld\n\nn = %lld\nphi = %lld\ne = %lld\n", p, q, n, phi, e);
+
+    char in[3], out[40];
+
+
     return 0;
 }
